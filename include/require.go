8c87177efc0b341f0a4cb112e96533bc3fb9dba7
zhangguojvn <zhangguojvn@gmail.com>
2023-04-24 14:48:09 +0800

    将头文件加入依赖

---
+++ include/require.go
@@ -0,0 +1,4 @@
+//go:build require
+// +build require
+
+package include
---
+++ require.go
@@ -0,0 +1,8 @@
+//go:build require
+// +build require
+
+package gobci
+
+import (
+	_ "github.com/zhangguojvn/gobci/include" // ODPI-C
+)
